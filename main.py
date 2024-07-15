8f1a08cf5ffca07363c336593b428f68950bbb66
Arpit-Gahlot <ar700601@dal.ca>
2024-07-15 16:24:22 +0530

    cannonball motion added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame import Vector2
+from math import cos, sin, radians
 
 # Define the variables
 BLOCK_WIDTH = 48
@@ -18,6 +19,13 @@ def rotate_on_pivot(image, angle, pivot, origin):
     offset = pivot + (origin - pivot).rotate(-angle)
     rect = surf.get_frect(center = offset)
     return surf, rect
+
+
+# function to rotate an object on a specified pivot, without rotating the image itself
+def rotate_on_pivot_still(image, angle, pivot, origin):
+    offset = pivot + (origin - pivot).rotate(-angle)
+    rect = image.get_frect(center = offset)
+    return rect
 
 
 # Class for the Tanks
@@ -44,7 +52,7 @@ class Tank(pygame.sprite.Sprite):
         self.turret = Turret(Vector2(self.pivot_x, self.pivot_y), starting_angle = 0)
 
     # Method to update the positions of the tank and turret
-    def update(self, move_left, move_right, rotate_up, rotate_down, dt):
+    def update(self, move_left, move_right, rotate_up, rotate_down, charging_launch, dt):
 
         # accelerate or decelerate the tank based on keys pressed
         if move_left:
@@ -70,8 +78,17 @@ class Tank(pygame.sprite.Sprite):
         self.pivot_y = self.tank.center[1] - 10
         turret_pivot = Vector2(self.pivot_x, self.pivot_y)
 
-        self.turret.move(turret_pivot, dt)
-        self.turret.rotate(rotate_up, rotate_down, dt)
+        self.turret.update(turret_pivot, rotate_up, rotate_down, dt)
+
+        # If the cannon-ball launch is ready, check if the player is charging it
+        if self.turret.launch_ready:
+            if charging_launch:
+                self.turret.charge_launch(dt)
+                self.turret.cannonball.check_max_speed()
+                self.turret.charged = True
+            else:
+                if self.turret.charged:
+                    self.turret.launch()
 
     # method to speed up the tank in the right direction
     def accelerate(self, dt):
@@ -134,6 +151,19 @@ class Turret:
         self.image_orig = reference_dict['LeftTankTurret']
         self.image = self.image_orig
         self.turret = self.image.get_frect(center=self.pos)
+        self.cannonball_offset = Vector2(20 * cos(radians(self.angle)), -20 * sin(radians(self.angle)))
+        self.cannonball_origin = self.turret.center + self.cannonball_offset
+        self.cannonball = Cannonball(self.cannonball_origin, self.angle)
+        self.launch_ready = True
+        self.charged = False
+
+    # Method to handle other methods of the turret and the cannonballs
+    def update(self, turret_pivot, rotate_up, rotate_down, dt):
+        self.move(turret_pivot)
+        self.rotate(rotate_up, rotate_down, dt)
+        self.cannonball.handle_projectile(dt)
+        self.cannonball.update_position(self.turret.center, self.angle)
+        self.cannonball.check_if_landed(self.pivot)
 
     # Method to handle the rotation of the turret
     def rotate(self, rotating_up, rotating_down, dt):
@@ -147,35 +177,89 @@ class Turret:
         self.image, self.turret = rotate_on_pivot(self.image_orig, self.angle, self.pivot, self.pos)
 
     # Method to move the turret's pivot point
-    def move(self, pivot, dt):
+    def move(self, pivot):
         self.pivot = Vector2(pivot)
         self.pos = self.pivot + self.offset
+
+    # Method to increase the cannonball launch speed if the user is charging it
+    def charge_launch(self, dt):
+        if not self.cannonball.launched:
+            self.cannonball.launch_speed += 100 * dt
+            self.cannonball.update_speed()
+
+    # Method to launch the cannonball
+    def launch(self):
+        self.cannonball.launch_angle = self.angle
+        self.cannonball.launched = True
+        self.charged = False
 
     # Method to draw the turret
     def draw(self, surface):
         pygame.draw.line(surface, 'red', (self.pivot.x, 0), (self.pivot.x, SCREEN_HEIGHT))
         pygame.draw.line(surface, 'red', (0, self.pivot.y), (SCREEN_WIDTH, self.pivot.y))
+        self.cannonball.draw(surface)
         surface.blit(self.image, self.turret)
 
 
-# Class for the Cannon-Balls that are launched from the turret
-class CannonBall:
-    # Minimum horizontal speed of the cannon-ball
-    min_horizontal_speed = 5
-
+# Class for the Cannonballs that are launched from the turret
+class Cannonball:
+    # Minimum and maximum speeds of the cannon-ball
+    min_speed = 250
+    max_speed = 500
     # acceleration due to gravity
-    g = 1
+    g = 5
 
-    def __init__(self, launch_point, offset_distance, launch_angle):
-        self.point = launch_point
+    def __init__(self, launch_point, launch_angle):
         self.launch_angle = launch_angle
-        offset = Vector2()
-        offset.from_polar((offset_distance, -launch_angle))
-        self.position = self.point + offset
-        image = reference_dict['CannonBall']
-        self.cannonball = image.get_frect(center = launch_point)
+        self.image = reference_dict['Cannonball']
+        self.cannonball = self.image.get_frect(center = launch_point)
+        self.launch_speed = self.min_speed
+        self.horizontal_speed = self.launch_speed * cos(radians(self.launch_angle))
+        self.vertical_speed = self.launch_speed * sin(radians(self.launch_angle))
+        self.launched = False
 
+    # update the position of the cannonball while it's loaded in the turret
+    def update_position(self, position, angle):
+        if not self.launched:
+            self.cannonball.center = position
+            cannonball_offset = Vector2(20 * cos(radians(angle)), -20 * sin(radians(angle)))
+            self.cannonball.center += cannonball_offset
+            self.launch_angle = angle
 
+    # update the position of the cannonball after it's launched
+    def handle_projectile(self, dt):
+        if self.launched:
+            current_x_position = self.cannonball.centerx
+            current_y_position = self.cannonball.centery
+            current_x_position += self.horizontal_speed * dt
+            current_y_position -= self.vertical_speed * dt
+            self.cannonball.center = Vector2(current_x_position, current_y_position)
+            print(self.horizontal_speed)
+            # Implement gravity
+            self.vertical_speed -= self.g
+
+    # Method to update the horizontal and vertical speed components of the turret while it is loaded in the turret
+    def update_speed(self):
+        self.horizontal_speed = self.launch_speed * cos(radians(self.launch_angle))
+        self.vertical_speed = self.launch_speed * sin(radians(self.launch_angle))
+
+    # Method to check that the cannonball isn't charged beyond the max speed
+    def check_max_speed(self):
+        if self.launch_speed > self.max_speed:
+            self.launch_speed = self.max_speed
+
+    # Method to check if the cannonball has landed
+    def check_if_landed(self, pivot):
+        if self.cannonball.centery > pivot.y:
+            self.launched = False
+            self.launch_speed = self.min_speed
+
+    # Method to draw the cannonball on the screen
+    def draw(self, surface):
+        pygame.draw.line(surface, 'green', (self.cannonball.centerx, 0), (self.cannonball.centerx, SCREEN_HEIGHT))
+        pygame.draw.line(surface, 'green', (0, self.cannonball.centery), (SCREEN_WIDTH, self.cannonball.centery))
+        if self.launched:
+            surface.blit(self.image, self.cannonball)
 
 
 # Class that handles the game logic
@@ -195,13 +279,14 @@ class Game:
         self.tank_moving_right = False
         self.turret_rotating_up = False
         self.turret_rotating_down = False
+        self.charging_launch = False
 
         # Add the required images to the reference dictionary
         self.scale = 0.2
         self.load_image('LeftTank', 'LeftTankTurret')
         for i in range(3):
             self.load_image('LeftTank', f'LeftTank{i}')
-        self.load_image('LeftTank', 'CannonBall')
+        self.load_image('LeftTank', 'Cannonball')
 
         # Create Tank object for the first player
         self.first_tank = Tank('LeftTank', 500, 300)
@@ -219,19 +304,17 @@ class Game:
     def update(self, dt):
         self.first_tank.update_animation()
         self.first_tank.update(self.tank_moving_left, self.tank_moving_right,
-                               self.turret_rotating_up, self.turret_rotating_down, dt)
+                               self.turret_rotating_up, self.turret_rotating_down, self.charging_launch, dt)
 
     # Method to draw the tank on the screen
     def draw(self, surface):
         surface.fill('black')
         self.first_tank.draw(surface)
 
-    # Method that handles the game loop
+    # Method that handles the game loop. It is called 60 times each second
     def run(self):
-
         run = True
         while run:
-
             # calculate delta time
             current_time = pygame.time.get_ticks() * 0.001
             dt = current_time - self.last_time
@@ -256,6 +339,8 @@ class Game:
                         self.turret_rotating_up = True
                     if event.key == pygame.K_DOWN:
                         self.turret_rotating_down = True
+                    if event.key == pygame.K_SPACE:
+                        self.charging_launch = True
                     # quit game
                     if event.key == pygame.K_ESCAPE:
                         run = False
@@ -270,6 +355,8 @@ class Game:
                         self.turret_rotating_up = False
                     if event.key == pygame.K_DOWN:
                         self.turret_rotating_down = False
+                    if event.key == pygame.K_SPACE:
+                        self.charging_launch = False
 
             pygame.display.update()
         pygame.quit()
